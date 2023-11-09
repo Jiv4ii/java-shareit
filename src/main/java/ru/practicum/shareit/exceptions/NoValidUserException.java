16f915efc09ed5a8650be7b7e2df67c30fca43bf
Jiv4ii <usaaalee@gmail.com>
2023-11-09 23:00:36 +0500

    Исправление синтаксиса

--- src/main/java/ru/practicum/shareit/exceptions/NoValidUserException.java
+++ src/main/java/ru/practicum/shareit/exceptions/NoValidUserException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exceptions;
 
-public class NoValidUserException extends RuntimeException{
+public class NoValidUserException extends RuntimeException {
     public NoValidUserException(String message) {
         super(message);
     }
