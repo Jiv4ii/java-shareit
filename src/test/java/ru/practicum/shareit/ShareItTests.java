6625cec2d6ee340fa9d05aedbe3828634c21a5fd
Jiv4ii <usaaalee@gmail.com>
2023-12-15 12:01:10 +0500

    Sprint 15.2 исправление синтаксиса

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,11 +1,8 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
 @SpringBootTest
